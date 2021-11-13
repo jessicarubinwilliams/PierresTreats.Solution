384b2defc1e760ec8cd37ed474f806d5a3885732
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-11-13 12:41:04 -0800

    Remove unused Namespaces

--- Bakery/Controllers/TreatsController.cs
+++ Bakery/Controllers/TreatsController.cs
@@ -6,8 +6,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace Bakery.Controllers
 {
